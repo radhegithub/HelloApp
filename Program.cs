1f61d6cf05fa7364c3a266f3a98ef001ba86403b
Radhe Sah <radhesah@gmail.com>
2020-04-11 12:07:51 +0530

    First commitradhe

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Console.WriteLine("Hello World!");
+            for (int i = 0; i <= 5; i++)
+                Console.WriteLine("Hello App " + i);
+        }
+    }
+}
+
